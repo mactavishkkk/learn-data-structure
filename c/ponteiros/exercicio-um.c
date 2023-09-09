29fe4952509ab7b64f0a178dfcea66bfac67900f
isaiasleite <isaiasleite230@gmail.com>
2023-09-09 10:12:01 -0400

    update

--- c/ponteiros/exercicio-um.c
+++ c/ponteiros/exercicio-um.c
@@ -5,7 +5,7 @@ int main()
 {
     int a, b, *p1, *p2;
 
-    a= 4;
+    a = 4;
     b = 3;
     p1 = &a;
     p2 = p1;
@@ -17,7 +17,7 @@ int main()
 
     printf("&a = %p, &b = %p\n", &a, &b);
     printf("&p1 = %p, &p2 = %p\n\n", &p1, &p2);
-    
+
     printf("a = %d, b = %d\n", a, b);
     printf("p1 = %p, p2 = %p\n", p1, p2);
     printf("*p1 = %d *p2 = %d\n", *p1, *p2);
